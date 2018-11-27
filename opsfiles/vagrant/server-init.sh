afae52abc20373db08f3baa16c5f2813ae3c301e
iganari <cigaguri@gmail.com>
2018-11-27 19:44:43 +0900

    update

--- opsfiles/vagrant/server-init.sh
+++ opsfiles/vagrant/server-init.sh
@@ -2,21 +2,25 @@
 
 set -x
 
-mkdir -p           /usr/local/iganari/notify-server
-chown root:root -R /usr/local/iganari
-chmod 0777      -R /usr/local/iganari
+export _user='notify-server'
 
-# groupadd hejda
-# useradd -m -s /bin/bash hejda-admin -G hejda
-useradd -m -s /bin/bash iganari
+useradd -m -s /bin/bash ${_user}
 
-echo 'iganari ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers.d/iganari
-chmod 0440                                  /etc/sudoers.d/iganari
-
-
-ls /develop/notify-server | grep -v opsfiles | xargs -i cp -vr /develop/notify-server/{} /usr/local/iganari/notify-server/
-                                                   
-chown root:root -R /usr/local/iganari
-chmod 0777      -R /usr/local/iganari
-
-ls -la /usr/local/iganari/notify-server/
+# mkdir -p           /usr/local/iganari/notify-server
+# chown root:root -R /usr/local/iganari
+# chmod 0777      -R /usr/local/iganari
+# 
+# # groupadd hejda
+# # useradd -m -s /bin/bash hejda-admin -G hejda
+# useradd -m -s /bin/bash iganari
+# 
+# echo 'iganari ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers.d/iganari
+# chmod 0440                                  /etc/sudoers.d/iganari
+# 
+# 
+# ls /develop/notify-server | grep -v opsfiles | xargs -i cp -vr /develop/notify-server/{} /usr/local/iganari/notify-server/
+#                                                    
+# chown root:root -R /usr/local/iganari
+# chmod 0777      -R /usr/local/iganari
+# 
+# ls -la /usr/local/iganari/notify-server/
